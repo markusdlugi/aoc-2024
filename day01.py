4ee585826fd35f9031070f2bd2ca9e13d4375941
Markus Dlugi <dlugi.markus@gmail.com>
2024-12-01 12:43:51 +0100

    Simplify

--- day01.py
+++ day01.py
@@ -1,28 +1,20 @@
-from collections import defaultdict
+from collections import Counter
 
 lines = list(open("input/01.txt"))
 
 left = []
 right = []
-left_count = defaultdict(int)
-right_count = defaultdict(int)
 for line in lines:
     a, b = map(int, line.split())
     left.append(a)
     right.append(b)
-    left_count[a] += 1
-    right_count[b] += 1
 
 left.sort()
 right.sort()
-
-distance = 0
-for i in range(len(lines)):
-    distance += abs(left[i] - right[i])
+distance = sum(abs(left[i] - right[i]) for i in range(len(lines)))
 print(distance)
 
-similarity_score = 0
-for key, val in left_count.items():
-    similarity_score += val * key * right_count[key]
-
+left_count = Counter(left)
+right_count = Counter(right)
+similarity_score = sum(val * key * right_count[key] for key, val in left_count.items())
 print(similarity_score)
